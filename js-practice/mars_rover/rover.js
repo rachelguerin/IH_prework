f017671333c07e8ffefe42c3771c9fad7e099cd0
Rachel Guerin <rachel.guerin23@gmail.com>
2016-01-25 20:48:34 +0100

    Mars Rover extras

--- js-practice/mars_rover/rover.js
+++ js-practice/mars_rover/rover.js
@@ -1,37 +1,73 @@
 var myRover = {
+  type: 'myRover',
   position: [0,0], 
   direction: 'N'
 };
+
+var sheRover = {
+  type: 'sheRover',
+  position: [1,0],
+  direction: 'N'
+};
+
+var rock = {
+  type: 'rock',
+  position: [2,0]
+};
+
+var hole = {
+  type: 'hole',
+  position: [5,5]
+};
+
+var myMap = {
+  dimensions: [9,9],
+  objects: [myRover,sheRover,rock,hole]
+}
+
+function checkForObstacles(map,position) {
+  for (i in map.objects) {
+    if (map.objects[i].position[0] === position[0] && map.objects[i].position[1] === position[1]){
+      console.log("There is a " + map.objects[i].type + " at [" + position + "].");
+      return true;
+    }
+  }
+
+  return false;
+
+}
 
 function printPosition(rover) {
   console.log("New Rover Position: [" + rover.position[0] + ", " 
                 + rover.position[1] + "] " + rover.direction);
 }
 
-function goForward(rover) {
+function goForward(rover,map) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
-      if (rover.position[0] > 9) {
-        rover.position[0] -= 10;
+      if (checkForObstacles(map,[rover.position[0]+1,rover.position[1]]) === false) {
+        rover.position[0]++
+        if (rover.position[0] > map.dimensions[0]) {
+          rover.position[0] -= map.dimensions[0]+1;
+        }
       }
       break;
     case 'E':
       rover.position[1]++
-      if (rover.position[1] > 9) {
-        rover.position[1] -= 10;
+      if (rover.position[1] > map.dimensions[1]) {
+        rover.position[1] -= map.dimensions[1]+1;
       }
       break;
     case 'S':
       rover.position[0]--
       if (rover.position[0] < 0) {
-        rover.position[0] += 10;
+        rover.position[0] += map.dimensions[0]+1;
       }
       break;
     case 'W':
       rover.position[1]--
       if (rover.position[1] < 0) {
-        rover.position[1] += 10;
+        rover.position[1] += map.dimensions[1]+1;
       }
       break;
   };
@@ -40,7 +76,7 @@ function goForward(rover) {
    
 }
 
-function goBack(rover){
+function goBack(rover,map){
   switch(rover.direction){
     case 'N':
       rover.position[0]--;
@@ -51,19 +87,19 @@ function goBack(rover){
     case 'E':
       rover.position[1]--;
       if (rover.position[1] < 0) {
-        rover.position[1] += 10;
+        rover.position[1] += map.dimensions[1]+1;
       }
       break;
     case 'S':
       rover.position[0]++;
-      if (rover.position[0] > 9) {
-        rover.position[0] -= 10;
+      if (rover.position[0] > map.dimensions[0]) {
+        rover.position[0] -= map.dimensions[0]+1;
       }
       break;
     case 'W':
       rover.position[1]++;
-      if (rover.position[1] > 9) {
-        rover.position[1] -= 10;
+      if (rover.position[1] > map.dimensions[1]) {
+        rover.position[1] -= map.dimensions[1]+1;
       }
       break;
   };
@@ -110,30 +146,33 @@ function turnLeft(rover) {
 }
 
 
-function moveRover(rover,directions) {
+function moveRover(rover,map,directions) {
   for (i in directions) {
     switch (directions[i]) {
       case 'f':
-        goForward(rover);
+        goForward(rover,map);
         break;
       case 'b':
-        goBack(rover);
+        goBack(rover,map);
         break;
       case 'r':
-        turnRight(rover);
+        turnRight(rover,map);
         break;
       case 'l':
-        turnLeft(rover);
+        turnLeft(rover,map);
         break;
     }
   }
 }
 
-// var myDirections = "fffrfflfffbb".split("");
-// moveRover(myRover,myDirections);
+// checkForObstacles(myMap,[5,5]);
+
+var myDirections = "fffrfflfffbb".split("");
+moveRover(myRover,myMap,myDirections);
+moveRover(sheRover,myMap,myDirections);
 
 // myDirections = "fffffffffffflffffffffffff".split("");
 // moveRover(myRover,myDirections);
 
-myDirections = "bbbbbrbbbbbb".split("");
-moveRover(myRover,myDirections);
+// myDirections = "bbbbbrbbbbbb".split("");
+// moveRover(myRover,myDirections);
